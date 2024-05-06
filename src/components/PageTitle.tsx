b9086905f49dcb60129adf50ea50196904933e4a
tatsuyashinsei <145214721+tatsuyashinsei@users.noreply.github.com>
2024-05-06 21:31:50 +0900

    Initial commit

---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+// import './App.css';
+import * as React from 'react';
+
+function PageTitle() {
+  return <h1>Hello</h1>;
+}
+
+// export default App;
+export default PageTitle;
